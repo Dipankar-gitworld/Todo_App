727fa26572804b69b0a48156c0a2f7a8d15598f9
Dipankar Sarkar <sarkar.dipankar9999@gmail.com>
2022-04-27 13:03:55 +0530

    updated db.js

--- backend/src/configs/db.js
+++ backend/src/configs/db.js
@@ -2,9 +2,9 @@
 const {connect,Promise} = require("mongoose");
 // Promise = global.Promise;
 require(`dotenv`).config();
-// const username = process.env.MONGO_USERNAME;
-// const password = process.env.MONGO_PASSWORD;
+const username = process.env.MONGO_USERNAME;
+const password = process.env.MONGO_PASSWORD;
 module.exports = ()=>{
-    return connect("mongodb+srv://dipankar10:dipankar100@cluster0.hmrsq.mongodb.net/TOdo?retryWrites=true&w=majority");
+    return connect(`mongodb+srv://${username}:${password}@cluster0.hmrsq.mongodb.net/TOdo?retryWrites=true&w=majority`);
 }
 
